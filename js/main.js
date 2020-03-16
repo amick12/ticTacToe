15c2b8661d685de2ff96d86b689e61aff79c6d91
amick12 <amckee26@gmail.com>
2020-03-16 11:54:22 -0400

    Adjust players/Add Classes

--- js/main.js
+++ js/main.js
@@ -7,20 +7,21 @@ let winner;
 let rowIdArray = [];
 let columnIdArray = [];
 let clickedTilesArray = [];
+let possibleWinsArray = [
+    [1, 2, 3], [4, 5, 6],
+    [7, 8, 9], [1, 5, 9],
+    [3, 5, 7], [1, 4, 7],
+    [2, 5, 8], [2, 6, 9]];
 
 //create constants for innerText of #winner & #score
 const WINNER_TEXT = document.getElementById('winner');
 const SCORE_TEXT = document.getElementById('score');
 
-//create constants equal to HTML
-const PLAYER_1_HTML = '<h1 class="text-center pt-4 mt-1 font-weight-bolder">X</h1>';
-const PLAYER_2_HTML = '<h1 class="text-center pt-4 mt-1 font-weight-bolder">O</h1>'
+/*create constants equal to HTML
+        **Done**--need to assign classes to differentiate x's tiles & o's tiles*/
 
-let possibleWinsArray = [
-    ['col1, col2, col3'], ['col4, col5, col6'],
-    ['col7, col8, col9'], ['col1, col5, col9'],
-    ['col3, col5, col7'], ['col1, col4, col7'],
-    ['col2, col5, col8'], ['col2, col6, col9']];
+const PLAYER_1_HTML = '<h1 class="x text-center pt-4 mt-1 font-weight-bolder">X</h1>';
+const PLAYER_2_HTML = '<h1 class="o text-center pt-4 mt-1 font-weight-bolder">O</h1>'
 
 const CONTAINER = document.createElement('div');
 CONTAINER.id = 'outerCont';
@@ -29,86 +30,84 @@ CONTAINER.classList.add('container');
 document.body.appendChild(CONTAINER);
 
 // function prototype for Row
-function Row(id, classList) {
-    this.id = id;
-    this.classList = classList;
+class Row {
+    constructor(id, classList) {
+        this.id = id;
+        this.classList = classList;
+    }
+    create() {
+        let row = document.createElement('div');
+        row.id = this.id;
+        row.classList = this.classList;
+        document.getElementById('outerCont').appendChild(row);
+    }
 }
 
-Row.prototype.create = function () {
-    let row = document.createElement('div');
-    row.id = this.id;
-    row.classList = this.classList;
-
-    document.getElementById('outerCont').appendChild(row);
-}
 
 // function prototype for Column
-function Column(id, classList, rowId) {
-    this.id = id;
-    this.classList = classList;
-    this.rowId = rowId;
-}
-
-Column.prototype.create = function () {
-    let column = document.createElement('div');
-    column.id = this.id;
-    column.classList = this.classList;
-    column.location = this.location;
-    column.rowId = this.rowId;
-
-    document.getElementById(column.rowId).appendChild(column);
-    /*
-       **DONE**--Something needs to happen when a column is clicked
-               **DONE**--addEventListener 'click' to columns dynamically
-               **DONE**--create onclick function to handle post click logic   
-   */
-
-    document.getElementById(column.id).addEventListener('click', gotClicked, { once: true });
-    /*  
-    
-    ***** create fx for column onclick <---> function gotClicked() below *****
- 
-            **DONE**-- check innerHTML for empty
-                    **DONE**--whose turn is it? 
-                    **DONE**--display h1 
-                        **DONE**--  X or O based on whose turn in current state
-            **DONE**--prevent reclick on same column
-                    **DONE**--use addEL third parameter? 
-                        **DONE**--once (a boolean, initially false) 
-            //TODO:
-            --  check to see if game has ended
-                    --has someone won?
-                    --has there been a draw?
-            --  if no one has one and there is no draw:
-                    --  add 1 to numberOfClicks
-                    --  add 1 to turn
-                
-    */
-    function gotClicked(e) {
-        let gotClickedCol = document.getElementById(this.id);
-
-        if (gotClickedCol.innerHTML === '') {
-            whoseTurnIsIt();
-            gotClickedCol.innerHTML = turn;
-            numOfClicks++;
-            clickedTilesArray.push(this.id);
+class Column {
+    constructor(id, classList, rowId) {
+        this.id = id;
+        this.classList = classList;
+        this.rowId = rowId;
+    }
+    create() {
+        let column = document.createElement('div');
+        column.id = this.id;
+        column.classList = this.classList;
+        column.location = this.location;
+        column.rowId = this.rowId;
+        document.getElementById(column.rowId).appendChild(column);
+        /*
+           **DONE**--Something needs to happen when a column is clicked
+                   **DONE**--addEventListener 'click' to columns dynamically
+                   **DONE**--create onclick function to handle post click logic
+       */
+        document.getElementById(column.id).addEventListener('click', gotClicked, { once: true });
+        /*
+        
+        ***** create fx for column onclick <---> function gotClicked() below *****
+     
+                **DONE**-- check innerHTML for empty
+                        **DONE**--whose turn is it?
+                        **DONE**--display h1
+                            **DONE**--  X or O based on whose turn in current state
+                **DONE**--prevent reclick on same column
+                        **DONE**--use addEL third parameter?
+                            **DONE**--once (a boolean, initially false)
+                //TODO:
+                --  check to see if game has ended
+                        --has someone won?
+                **DONE**--has there been a draw?
+                --  if no one has won and there is no draw:
+                        --  add 1 to numberOfClicks
+                        --  add 1 to turn
+                    
+        */
+        function gotClicked(e) {
+            let gotClickedCol = document.getElementById(this.id);
+            if (gotClickedCol.innerHTML === '') {
+                whoseTurnIsIt();
+                gotClickedCol.innerHTML = turn;
+                numOfClicks++;
+                clickedTilesArray.push(this.id);
+            }
         }
     }
-
 }
+
 
 //create function to check if the game has ended
 // function hasGameEnded() {
 
 // }
 
-//create function to check if the game is a draw
-// function isDraw() {
-//     if() {
-
-//     }
-// }
-
+// create function to check if the game is a draw
+function isDraw() {
+    if (numOfClicks === 9) {
+        gameOver;
+    }
+}
 // create fx to decide whose turn it is @ 'click'
 function whoseTurnIsIt() {
     if (numOfClicks % 2 !== 0) {
@@ -123,6 +122,10 @@ function whoseTurnIsIt() {
 //     if (possibleWinsArray)
 // }
 
+//create function for gameOver logic
+// function gameOver() {
+
+// }
 
 //function to create row --> for loop for multiple rows
 
